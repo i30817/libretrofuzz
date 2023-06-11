674a249310f317fad7050c67e32bced48b5a30af
i30817 <i30817@gmail.com>
2023-06-11 02:52:51 +0100

    Use Token ratio and use a heuristic where perfect matches discourage matching again

--- libretrofuzz/__main__.py
+++ libretrofuzz/__main__.py
@@ -284,7 +284,7 @@ def which(executable):
 # or less to the local labels (after the normalization)
 # -------------------------------------------------------------------
 class TitleScorer(object):
-    def __init__(self, subtitles):
+    def __init__(self, subtitles, other_subtitles):
         # rapidfuzz says to use range 0-100, but this doesn't (it's much easier that way)
         # so it uses internal api to prevent a possible early exit at == 100
         self._RF_ScorerPy = {
@@ -295,20 +295,27 @@ class TitleScorer(object):
             }
         }
         self.subtitles = subtitles
+        self.other_subtitles = other_subtitles
 
     def check_full_match(self, name, other_name):
+        if name == other_name:
+            return 200
+        name_ns = "".join(name.split())
+        if name_ns == "".join(other_name.split()):
+            return 200
         candidates = []
-        other_subs = self.subtitles[other_name]
-        for sub in other_subs:
-            if name == sub:
-                candidates.append(200 - len(other_subs) + 1)
-            else:
-                len_ratio = min(len(name), len(sub)) / max(len(name), len(sub))
-                heuristic = len(os.path.commonprefix([name, sub])) * len_ratio
-                candidates.append(fuzz.WRatio(name, sub) + heuristic)
-        len_ratio = min(len(name), len(other_name)) / max(len(name), len(other_name))
-        heuristic = len(os.path.commonprefix([name, other_name])) * len_ratio
-        candidates.append(fuzz.WRatio(name, other_name) + heuristic)
+        other_subs = self.other_subtitles[other_name]
+        # If a playlist has 'other name' that means 'other name' is
+        # a perfect match to another game on the playlist.
+        # that probably means it's not the right game
+        perfect_for_another = 0.1 if other_name in self.subtitles else 0
+        if len(other_subs) > 1:
+            for sub in other_subs:
+                if name == sub or name_ns == "".join(sub.split()):
+                    candidates.append(200 - perfect_for_another)
+                else:
+                    candidates.append(fuzz.token_ratio(name, sub) - perfect_for_another)
+        candidates.append(fuzz.token_ratio(name, other_name) - perfect_for_another)
         return max(candidates)
 
     def __call__(self, s1, s2, processor=None, score_cutoff=None):
@@ -420,8 +427,8 @@ def normalizer(nometa, hack, t):
         st = st.replace("i", "1")
         # remove diacritics (not to asian languages diacritics, only for 2 to 1 character combinations)
         st = "".join([c for c in unicodedata.normalize("NFKD", st) if not unicodedata.combining(c)])
-        st = " ".join(st.strip().split())
-        subtitles[i] = st
+        st = st.strip().split()
+        subtitles[i] = " ".join(st)
     return " ".join(subtitles), subtitles
 
 
@@ -952,19 +959,35 @@ async def downloader(
     # (until you call again without --no-merge anyway)
     remote_names = set()
     remote_names.update(thumbs.Named_Boxarts.keys(), thumbs.Named_Titles.keys(), thumbs.Named_Snaps.keys())
-    # preprocess data for speed and to make
-    # exact matches (for total name or subtitle)
-    # not involved in the fuzz function.
-    # turn into a dict, original key and
-    # (normalized value, [possible subtitles normalized nospace])
-    subtitle = dict()
-    mappings = dict()
+
+    # preprocess data to build a heuristic later. Do not move
+    # into the later loop because thats when the heuristic is used
+    subtitles1 = dict()
+    subtitles2 = dict()
+    mappings1 = dict()
+    mappings2 = dict()
     for x in remote_names:
         normstr, lst = norm(x)
-        subtitle[normstr] = lst
-        mappings[x] = normstr
-    remote_names = mappings
-    title_scorer = TitleScorer(subtitle)
+        subtitles2[normstr] = lst
+        mappings1[x] = normstr
+    remote_names = mappings1
+    # this loop is used to build a heuristic explained in titlescorer (dont merge it)
+    for name, _ in names:
+        name = regex.sub(forbidden, "_", name)
+        norm_name = name
+        if before:
+            # Ignore metadata and get the string before it
+            name_without_meta = regex.search(r"(^[^\[({]*)", norm_name)
+            if name_without_meta:
+                before_index = name_without_meta.group(1).find(before)
+                if before_index != -1:
+                    norm_name = norm_name[0:before_index]
+
+        (norm_name, namesubs) = norm(norm_name)
+        subtitles1[norm_name] = namesubs
+        mappings2[name] = norm_name
+
+    title_scorer = TitleScorer(subtitles1, subtitles2)
 
     for name, destination in names:
         await asyncio.sleep(0)  # update key status
@@ -972,34 +995,13 @@ async def downloader(
         # if the user used filters, filter everything that doesn't match any of the globs
         if filters and not any(map(lambda x: fnmatch.fnmatch(name, x), filters)):
             continue
-
-        # to simplify this code, the forbidden characters are replaced twice, on the string
-        # that is going to be the filename and the string copy that is going to be matched.
-        nameaux = name
-        #'before' has priority over subtitle removal
-        if before:
-            # Ignore metadata and get the string before it
-            name_without_meta = regex.search(r"(^[^\[({]*)", nameaux)
-            if name_without_meta:
-                before_index = name_without_meta.group(1).find(before)
-                if before_index != -1:
-                    nameaux = nameaux[0:before_index]
-
         # only the local names should have forbidden characters
-        # this is the character libretro server uses to replace
         name = regex.sub(forbidden, "_", name)
-        nameaux = regex.sub(forbidden, "_", nameaux)
-
-        # unlike the server thumbnails, normalization wasn't done yet
-        (nameaux, namesubs) = norm(nameaux)
-        if nameaux not in subtitle:
-            subtitle[nameaux] = namesubs
-
+        nameaux = mappings2[name]
         # normalization can make it so that the winner has the same score as the runner up(s)
-        # so to make sure we catch at least two candidates for cases where that happens
-        # it's a improvement because sometimes server thumbnail types have case letter typos
         result = process.extract(nameaux, remote_names, scorer=title_scorer, limit=verbose or 2)
-        _, max_score, _ = (result and result[0]) or (None, -1, None)
+        assert result and len(result) > 0
+        _, max_score, _ = result[0]
         winners = [x for x in result if x[1] == max_score and x[1] >= score]
         show = result if verbose else winners
         name_format = style((nameaux if short_names else name) + ": ", bold=True)
@@ -1128,8 +1130,8 @@ async def printwait(wait: Optional[float], waiting_format: str):
 def strfy(required_score, short_names, nub_verbose, r, urlsdict=None):
     thumb_norm, thumb_score, thumb_name = r
     score_color = RED if thumb_score < required_score else GREEN
-    # thumb_score = thumb_score if short_names else int(thumb_score)
-    score_text = style(f"{thumb_score}", fg=f"{score_color}", bold=True)
+    thumb_magnt = f"{thumb_score:.6f}" if short_names else f"{thumb_score:.2f}"
+    score_text = style(thumb_magnt, fg=score_color, bold=True)
     if nub_verbose:
         return f"{score_text} {thumb_norm}"
     elif urlsdict:
