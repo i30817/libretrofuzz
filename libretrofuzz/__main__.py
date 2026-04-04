00d49b72acc446174235cb05d557fff384b8913b
i30817 <i30817@gmail.com>
2026-04-04 21:59:24 +0100

    Update __main__.py

--- libretrofuzz/__main__.py
+++ libretrofuzz/__main__.py
@@ -888,7 +888,7 @@ def mainfuzzsingle(
 def mainfuzzall(
     cfg: Path = Argument(
         CONFIG,
-        help="Path to the retroarch cfg file. If not default, asked from the user.",
+        help="Path to the retroarch cfg file. If not defau	lt, asked from the user.",
         exists=True,
         file_okay=True,
         dir_okay=False,
@@ -1115,6 +1115,7 @@ async def rename_labels_in_playlist(
     verbose,
     scorer,
     limit,
+    filters,
 ):
     """Rename playlist labels using fuzzy matching scores.
 
@@ -1127,6 +1128,10 @@ async def rename_labels_in_playlist(
 
     for idx, name in enumerate(names):
         await exitcheck()
+        # if the user used filters, filter everything that doesn't match any filter
+        if filters and not any(map(lambda x: fnmatch.fnmatch(name, x), filters)):
+            unchanged_count += 1
+            continue
         result = process.extract(name, remote_names, scorer=scorer, limit=limit or 1)
         if result:
             _, best_score, _ = result[0]
@@ -1268,7 +1273,7 @@ async def downloader(
     if rename_labels:
         await rename_labels_in_playlist(
             names, remote_names, score, dryrun,
-            playlist_path, verbose, scorer, limit
+            playlist_path, verbose, scorer, limit, filters
         )
         return
     for name, destination in zip(names, dbs):
